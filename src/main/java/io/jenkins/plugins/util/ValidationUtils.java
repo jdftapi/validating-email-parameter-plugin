b6a83852b560cb16ac5e2b5524a6b926208c4266
Jonathan Dietrich <j.dietrich@ftapi.com>
2021-07-07 10:49:52 +0200

    refactor: use apache EmailValidator instead of Regex and refactor logic

--- src/main/java/io/jenkins/plugins/util/ValidationUtils.java
+++ src/main/java/io/jenkins/plugins/util/ValidationUtils.java
@@ -1,14 +1,11 @@
 package io.jenkins.plugins.util;
 
 import hudson.util.FormValidation;
-import io.jenkins.plugins.constant.Regex;
-import java.util.EnumMap;
 import net.sf.json.JSONObject;
 import org.apache.commons.validator.routines.EmailValidator;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public abstract class ValidationUtils {
 
@@ -17,26 +14,25 @@ public abstract class ValidationUtils {
     private static List<String> params;
     /*private static Map<String,String> paramMap;*/
 
+    private static final EmailValidator emailValidator = EmailValidator.getInstance();
+
+    private static boolean isMailAddressInDomain(String emailAddress, String domain) {
+        String[] addressParts = emailAddress.split("@");
+        return addressParts[addressParts.length - 1].equals(domain);
+    }
 
     public static JSONObject buildParameter(String domain, Boolean externalEmail,
                                             JSONObject jsonObject) {
 
         builder = new StringBuilder();
-
         normalizeSpace = jsonObject.getString(Key.VALUE.getValue()).trim();
-
         params = Arrays.asList(normalizeSpace.split(" "));
-
-        String domainRegEx = Regex.Domain.DOMAIN_PATTERN
-                .replace(Regex.Domain.DOMAIN_NAME, domain);
 
         if (!params.isEmpty()) {
             for (String s : params) {
-
-                if (Pattern.matches(Regex.EMAIL_PATTERN, s)) {
+                if (emailValidator.isValid(s)) {
                     if (!externalEmail) {
-
-                        if (Pattern.matches(domainRegEx, s)) {
+                        if (isMailAddressInDomain(s, domain)) {
                             builder.append(s).append(" ");
                         } else {
                             jsonObject.put(Key.IS_VALID.getValue(), false);
@@ -46,169 +42,102 @@ public abstract class ValidationUtils {
                             return jsonObject;
                         }
                     } else {
-
                         builder.append(s).append(" ");
                     }
                 } else if (!s.isEmpty()) {
-                    if (!externalEmail) {
-
-                        if (Pattern.matches(domainRegEx, (s + "@" + domain))) {
-
-                            builder.append(s).append("@").append(domain).append(" ");
-
-                        } else {
-                            jsonObject.put(Key.IS_VALID.getValue(), false);
-                            jsonObject.put(Key.ERROR.getValue(),
-                                    "Invalid email for parameter [" + jsonObject.getString("name")
-                                            + "] expected domain : @" + domain + " specified: " + s);
-                            return jsonObject;
-                        }
+                    if (emailValidator.isValid(s + "@" + domain)) {
+                        builder.append(s).append("@").append(domain).append(" ");
                     } else {
-
-                        if (Pattern.matches(Regex.EMAIL_PATTERN, (s + "@" + domain))) {
-
-                            builder.append(s).append("@").append(domain).append(" ");
-
-                        } else {
-                            jsonObject.put(Key.IS_VALID.getValue(), false);
+                        jsonObject.put(Key.IS_VALID.getValue(), false);
+                        if(!externalEmail) {
                             jsonObject.put(Key.ERROR.getValue(),
-                                    "Invalid email for parameter [" + jsonObject.getString("name")
-                                            + "] Value entered does not match "
-                                            + "email pattern; specified: " + s);
-                            return jsonObject;
+                               "Invalid email for parameter [" + jsonObject.getString("name")
+                                 + "] expected domain : @" + domain + " specified: " + s);
+                        } else {
+                            jsonObject.put(Key.ERROR.getValue(),
+                                "Invalid email for parameter [" + jsonObject.getString("name")
+                                   + "] Value entered does not match "
+                                   + "email pattern; specified: " + s);
                         }
+                        return jsonObject;
                     }
-                } else if (s.isEmpty()) {
-
-                    jsonObject.put(Key.IS_VALID.getValue(), false);
-                    jsonObject.put(Key.ERROR.getValue(),
-                            "Invalid email for parameter [ " + jsonObject.getString("name")
-                                    + " ] Expected domain : @" + domain + " Specified: [ Email is Empty! ]");
-                    return jsonObject;
                 } else {
                     jsonObject.put(Key.IS_VALID.getValue(), false);
                     jsonObject.put(Key.ERROR.getValue(),
-                            "Value entered does not match email pattern; Expected domain : @" + domain
-                                    + " Specified: " + s);
+                        "Invalid email for parameter [ " + jsonObject.getString("name")
+                            + " ] Expected domain : @" + domain
+                            + " Specified: [ Email is Empty! ]");
                     return jsonObject;
                 }
-
             }
-
             jsonObject.replace(Key.VALUE.getValue(), String.valueOf(builder).trim());
-            jsonObject.put(Key.IS_VALID.getValue(), true);
-
-            return jsonObject;
-        } else {
-            jsonObject.put(Key.IS_VALID.getValue(), true);
-            return jsonObject;
         }
-
+        jsonObject.put(Key.IS_VALID.getValue(), true);
+        return jsonObject;
     }
 
     public static String definitionParameter(String defaultValue, String domain) {
 
         builder = new StringBuilder();
-
         normalizeSpace = defaultValue.trim();
-
         params = Arrays.asList(normalizeSpace.split(" "));
 
         if (!params.isEmpty()) {
             for (String value : params) {
-
-                if (Pattern.matches(Regex.EMAIL_PATTERN, value)) {
+                if (emailValidator.isValid(value)) {
                     builder.append(value).append(" ");
                 } else if (!value.isEmpty()) {
-
                     builder.append(value).append("@").append(domain).append(" ");
                 } else {
                     return defaultValue;
                 }
-
             }
-
             return String.valueOf(builder).trim();
         } else {
-
             return defaultValue;
-
         }
-
     }
 
     public static FormValidation formParameter(String domain, String value, Boolean externalEmail) {
-
         builder = new StringBuilder();
-
         normalizeSpace = value.trim();
-
         params = Arrays.asList(normalizeSpace.split(" "));
-
-        String domainRegEx = Regex.Domain.DOMAIN_PATTERN
-                .replace(Regex.Domain.DOMAIN_NAME, domain);
 
         if (!params.isEmpty()) {
             for (String s : params) {
-
-                if (Pattern.matches(Regex.EMAIL_PATTERN, s)) {
-
+                if (emailValidator.isValid(s)) {
                     if (!externalEmail) {
-
-                        if (Pattern.matches(domainRegEx, s)
-                                && Pattern.matches(Regex.EMAIL_PATTERN, s)) {
+                        if (isMailAddressInDomain(s, domain)) {
                             builder.append(s).append(" ");
                         } else {
-                            return FormValidation
-                                    .error("Value entered does not match domain : " + s);
+                            return FormValidation.error("Value entered does not match domain : " + s);
                         }
                     } else {
-
                         builder.append(s).append(" ");
                     }
                 } else if (!s.isEmpty()) {
-                    if (!externalEmail) {
-
-                        if (Pattern.matches(domainRegEx, (s + "@" + domain))) {
-
-                            builder.append(s).append("@").append(domain).append(" ");
-
-                        } else {
-                            return FormValidation
-                                    .error("Value entered does not match domain : " + s);
-                        }
+                    if (emailValidator.isValid(s + "@" + domain)) {
+                        builder.append(s).append("@").append(domain).append(" ");
                     } else {
-
-                        if (Pattern.matches(Regex.EMAIL_PATTERN, (s + "@" + domain))) {
-
-                            builder.append(s).append("@").append(domain).append(" ");
-
+                        if (!externalEmail) {
+                            return FormValidation.error("Value entered does not match domain : " + s);
                         } else {
-                            return FormValidation
-                                    .error("Value entered does not match email address pattern : " + s);
+                            return FormValidation.error(
+                                "Value entered does not match email address pattern : " + s);
                         }
                     }
-
-                } else if (s.isEmpty()) {
-                    return FormValidation.warning("Email address is empty.");
                 } else {
-                    return FormValidation.error("Value entered does not match email pattern : " + s);
+                    return FormValidation.warning("Email address is empty.");
                 }
-
             }
-
             return FormValidation.respond(FormValidation.Kind.OK,
                     "<div class='ok' style='color:green;'><span>&#9989;</span><b> Valid Email : </b>"
                             + builder + "</div>");
-
         } else {
-
             return FormValidation.respond(FormValidation.Kind.OK,
                     "<div class='ok' style='color:green;'><span>&#9989;</span><b> Valid Email : </b>" + value
                             + "</div>");
-
         }
-
     }
 
     public enum Key {
@@ -218,7 +147,7 @@ public abstract class ValidationUtils {
 
         private final String value;
 
-        private Key(String value) {
+        Key(String value) {
             this.value = value;
         }
 
